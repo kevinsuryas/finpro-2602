31934a9797cc9c88f2270be51d8599c47c54772f
immanueljanis <immanuelj4nis@gmail.com>
2024-02-19 13:47:43 +0700

    build: create database schema that can provided next-auth

--- apps/web/src/app/api/auth/[...nextauth]/route.ts
+++ apps/web/src/app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,36 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth, { AuthOptions } from "next-auth";
-import GitHubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google";
+import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
 
-const authOption: AuthOptions = {
+export const authOption: AuthOptions = {
     providers: [
-        GitHubProvider({
-            clientId: process.env.GITHUB_CLIENT as string,
-            clientSecret: process.env.GITHUB_SECRET as string
-        }),
         GoogleProvider({
             clientId: process.env.GOOGLE_ID as string,
-            clientSecret: process.env.GOOGLE_SECRET as string
+            clientSecret: process.env.GOOGLE_SECRET as string,
+            profile(profile: GoogleProfile) {
+                return {
+                    id: profile.sub,
+                    name: profile.name,
+                    email: profile.email,
+                    image: profile.picture,
+                    role: profile.role ?? "customer",
+                }
+            }
         })
     ],
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) token.role = user.role
+            return token
+        },
+        async session({ session, token }) {
+            if (session?.user) session.user.role = token.role
+            return session
+        }
+    },
     secret: process.env.NEXTAUTH_SECRET
 }
 
-export const handler = NextAuth(authOption)
+const handler = NextAuth(authOption)
 
 export { handler as GET, handler as POST }
---
+++ apps/web/src/app/login/page.tsx
@@ -0,0 +1,7 @@
+import AuthButton from "@/components/navbar/AuthButton";
+
+export default function Login() {
+    return (
+        <AuthButton />
+    )
+}
